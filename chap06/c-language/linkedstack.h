e837944274550b1eb7e23c15bc7a8be0cc0a4930
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-01-05 19:20:20 +0900

    linkedstack headerfile

---
+++ chap06/c-language/linkedstack.h
@@ -0,0 +1,22 @@
+#ifndef C_LANGUAGE_LINKEDSTACK_H
+#define C_LANGUAGE_LINKEDSTACK_H
+
+typedef struct LinkedStackNodeType {
+    char data;
+    struct LinkedStackNode *pLink;
+} LinkedStackNode;
+
+typedef struct LinkedStackType {
+    int currentCount;
+    LinkedStackNode *pTop; // 탑 노드의 포인터
+} LinkedStack;
+
+LinkedStack *createLinkedStack();
+int pushLS(LinkedStack *pStack, char data);
+int isLinkedStackEmpty(LinkedStack *pStack);
+LinkedStackNode *popLS(LinkedStack *pStack);
+LinkedStackNode *peekLS(LinkedStack *pStack);
+void deleteLinkedStack(LinkedStack *pStack);
+void displayLinkedStack(LinkedStack *pStack);
+
+#endif
