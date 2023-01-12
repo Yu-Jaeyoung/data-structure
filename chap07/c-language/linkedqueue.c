640769a4d162e5b9022115db0bd931265d3572e5
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-01-13 00:23:48 +0900

    linkedqueue Example

---
+++ chap07/c-language/linkedqueue.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct LinkedQueueNodeType {
+    char data;
+    struct LinkedQueueNodeType *pLink;
+} LinkedQueueNode;
+
+typedef struct LinkedQueueType {
+    int currentCount; // 현재 노드의 개수
+    LinkedQueueNode *pFront; // Front 노드의 포인터
+    LinkedQueueNode *pRear; // Rear 노드의 포인터
+} LinkedQueue;
+
+int isLinkedQueueEmpty(LinkedQueue *pQueue) {
+    int ret = 0;
+
+    if (pQueue != NULL) {
+        if (pQueue->currentCount == 0) {
+            ret = 1;
+        }
+    }
+    return ret;
+}
+
+LinkedQueue *createLinkedQueue() {
+    LinkedQueue *pReturn = (LinkedQueue *) malloc(sizeof(LinkedQueue));
+    memset(pReturn, 0, sizeof(LinkedQueue));
+
+    return pReturn;
+}
+
+int enqueueLQ(LinkedQueue *pQueue, char data) {
+    int ret = 0;
+    LinkedQueueNode *pNode = NULL;
+
+    pNode = (LinkedQueueNode *) malloc(sizeof(LinkedQueueNode));
+    if (pNode != NULL) {
+
+        pNode->data = data;
+        pNode->pLink = NULL;
+
+        if (isLinkedQueueEmpty(pQueue) == 0) {
+            pQueue->pRear->pLink = pNode;
+            pQueue->pRear = pNode;
+        } else {
+            pQueue->pFront = pNode;
+            pQueue->pRear = pNode;
+        }
+        pQueue->currentCount++;
+        ret = 1;
+
+        return ret;
+    }
+
+    return ret;
+}
+
+LinkedQueueNode *dequeueLQ(LinkedQueue *pQueue) {
+    LinkedQueueNode *pReturn = NULL;
+    if (isLinkedQueueEmpty(pQueue) == 0) {
+        pReturn = pQueue->pFront;
+        pQueue->pFront = pQueue->pFront->pLink;
+        pReturn->pLink = NULL;
+
+        pQueue->currentCount--;
+    }
+
+    if (isLinkedQueueEmpty(pQueue) == 1) {
+        pQueue->pRear = NULL;
+    }
+    return pReturn;
+}
+
+LinkedQueueNode *peekLQ(LinkedQueue *pQueue) {
+    LinkedQueueNode *pReturn = NULL;
+    if (pQueue != NULL) {
+        if (isLinkedQueueEmpty(pQueue) == 0) {
+            pReturn = pQueue->pFront;
+        }
+    }
+    return pReturn;
+}
+
+void deleteLinkedQueue(LinkedQueue *pQueue) {
+    LinkedQueueNode *pNode = NULL;
+    LinkedQueueNode *pDelNode = NULL;
+
+    if (pQueue != NULL) {
+        pNode = pQueue->pFront;
+        while (pNode != NULL) {
+            pDelNode = pNode;
+            pNode = pNode->pLink;
+            free(pDelNode);
+        }
+        free(pQueue);
+    }
+}
+
+void displayLinkedQueue(LinkedQueue *pQueue) {
+    LinkedQueueNode *pNode = NULL;
+    int i = 0;
+    if (pQueue != NULL) {
+        printf("현재 노드 개수 : %d\n", pQueue->currentCount);
+        pNode = pQueue->pFront;
+        while (pNode != NULL) {
+            printf("[%d]-[%c]\n", i, pNode->data);
+            i++;
+            pNode = pNode->pLink;
+        }
+    }
+}
+
+int main() {
+    LinkedQueue *pQueue = NULL;
+    LinkedQueueNode *pNode = NULL;
+
+    pQueue = createLinkedQueue();
+    if (pQueue != NULL) {
+        enqueueLQ(pQueue, 'A');
+        enqueueLQ(pQueue, 'B');
+        enqueueLQ(pQueue, 'C');
+        enqueueLQ(pQueue, 'D');
+        displayLinkedQueue(pQueue);
+
+        pNode = dequeueLQ(pQueue);
+        if (pNode != NULL) {
+            printf("Dequeue Value-[%c]\n", pNode->data);
+            free(pNode);
+        }
+
+        displayLinkedQueue(pQueue);
+
+        pNode = peekLQ(pQueue);
+        if (pNode != NULL) {
+            printf("Peek Value-[%c]\n", pNode->data);
+        }
+        displayLinkedQueue(pQueue);
+
+        enqueueLQ(pQueue, 'E');
+
+        displayLinkedQueue(pQueue);
+    }
+    return 0;
+}
