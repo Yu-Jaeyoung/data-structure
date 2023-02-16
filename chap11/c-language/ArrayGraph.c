c6d423cabb481c64be8616b2c3fcdf90224cb829
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-02-16 15:27:04 +0900

    undirect array graph example

---
+++ chap11/c-language/ArrayGraph.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ARRAY_GRAPH_TYPE_DIRECT 0
+#define ARRAY_GRAPH_TYPE_UNDIRECT 1
+
+typedef struct ArrayGraphType {
+    int graphType; // 그래프의 종류 : 방향 그래프, 무방향 그래프
+    int nodeCount; // 노드 개수
+    int **ppEdge; // 간선 저장을 위한 2차원 array
+} ArrayGraph;
+
+ArrayGraph *createArrayGraph(int graphType, int nodeCount) {
+    int i = 0;
+    ArrayGraph *pReturn = NULL;
+    pReturn = (ArrayGraph *) malloc(sizeof(ArrayGraph));
+    if (pReturn == NULL) {
+        return NULL;
+    }
+
+    pReturn->graphType = graphType;
+    pReturn->nodeCount = nodeCount;
+
+    pReturn->ppEdge = (int **) malloc(sizeof(int *) * nodeCount);
+    if (pReturn->ppEdge == NULL) {
+        return NULL;
+    }
+    for (i = 0; i < nodeCount; i++) {
+        pReturn->ppEdge[i] = (int *) malloc(sizeof(int) * nodeCount);
+        if (pReturn->ppEdge[i] == NULL) {
+            return NULL;
+        }
+        memset(pReturn->ppEdge[i], 0, sizeof(int) * nodeCount);
+    }
+    return pReturn;
+}
+
+int checkVertexValid(ArrayGraph *pGraph, int node) {
+    if (pGraph != NULL && node < pGraph->nodeCount && node >= 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int addEdgeInternalAG(ArrayGraph *pGraph, int fromNode, int toNode) {
+    int ret = 0;
+
+    if (pGraph != NULL && checkVertexValid(pGraph, fromNode) && checkVertexValid(pGraph, toNode)) {
+        pGraph->ppEdge[fromNode][toNode] = 1;
+    } else {
+        ret = -1;
+    }
+    return ret;
+}
+
+int addEdgeAG(ArrayGraph *pGraph, int fromNode, int toNode) {
+    int ret = 0;
+
+    ret = addEdgeInternalAG(pGraph, fromNode, toNode);
+    if (0 == ret && ARRAY_GRAPH_TYPE_UNDIRECT == pGraph->graphType) {
+        ret = addEdgeInternalAG(pGraph, toNode, fromNode);
+    }
+    return ret;
+}
+
+int removeEdgeInternalAG(ArrayGraph *pGraph, int fromNode, int toNode) {
+    int ret = 0;
+    if (pGraph != NULL && checkVertexValid(pGraph, fromNode) && checkVertexValid(pGraph, toNode)) {
+        pGraph->ppEdge[fromNode][toNode] = 0;
+    } else {
+        ret = -1;
+    }
+    return ret;
+}
+
+int removeEdgeAG(ArrayGraph *pGraph, int fromNode, int toNode) {
+    int ret = 0;
+    ret = removeEdgeInternalAG(pGraph, fromNode, toNode);
+    if (0 == ret && ARRAY_GRAPH_TYPE_UNDIRECT == pGraph->graphType) {
+        ret = removeEdgeInternalAG(pGraph, toNode, fromNode);
+    }
+    return ret;
+}
+
+
+int getEdgeAG(ArrayGraph *pGraph, int fromNode, int toNode) {
+    int ret = 0;
+    if (pGraph != NULL && checkVertexValid(pGraph, fromNode) && checkVertexValid(pGraph, toNode)) {
+        ret = pGraph->ppEdge[fromNode][toNode];
+    }
+    return ret;
+}
+
+void displayGraphAG(ArrayGraph *pGraph) {
+    int i, j;
+    int count = 0;
+    if (pGraph != NULL) {
+        count = pGraph->nodeCount;
+        for (i = 0; i < count; i++) {
+            for (j = 0; j < count; j++) {
+                printf("%d ", getEdgeAG(pGraph, i, j));
+            }
+            printf("\n");
+        }
+    }
+}
+
+void deleteGraphAG(ArrayGraph *pGraph) {
+    int i = 0;
+    if (pGraph != NULL) {
+        for (i = 0; i < pGraph->nodeCount; i++) {
+            free(pGraph->ppEdge[i]);
+        }
+        free(pGraph->ppEdge);
+        free(pGraph);
+    }
+}
