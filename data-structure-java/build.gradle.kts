6332cba401197db38816e2a5760a90833ff77d3b
Yu-Jaeyoung <jackyu0721@gmail.com>
2025-07-16 22:40:38 +0900

    feat: Configure Gradle for unit testing
    - Add JUnit Jupiter API and Engine dependencies for testing.
    - Set up `test` task to run unit tests.

---
+++ data-structure-java/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+}
+
+group = "io.wisoft"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
